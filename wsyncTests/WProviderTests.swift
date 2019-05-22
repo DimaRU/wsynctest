001c5b66de75772ef76fad56062df3a60db51f6d
Dmitriy Borovikov <dmitry@borovikov.name>
2019-05-22 14:26:56 +0300

    Enhance WProviderTests

--- wsyncTests/WProviderTests.swift
+++ wsyncTests/WProviderTests.swift
@@ -13,6 +13,14 @@ class WProviderTests: XCTestCase {
         WAPI.getRoot()
             .done { root in
                 XCTAssertEqual(root.revision, 25830, "Revision not equal 25830, \(root)")
+            }.then {
+                WAPI.get(WList.self)
+            }.done { lists in
+                XCTAssertEqual(lists.count, 4, "List count must be 4")
+            }.then {
+                WAPI.get(WTask.self, id: 3858184298)
+            }.done { task in
+                XCTAssertTrue(task ==== wdump.tasks[3858184298]!, "Tasks must be equal")
             }.ensure {
                 expectation.fulfill()
             }.catch { error in
