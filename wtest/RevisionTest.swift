c4723a479191688799687010be456f16f85bb19b
Dmitriy Borovikov <dmitry@borovikov.name>
2019-06-08 15:45:40 +0300

    Delete test

--- wtest/RevisionTest.swift
+++ wtest/RevisionTest.swift
@@ -28,6 +28,10 @@ class RevisionTest {
         return WAPI.create(T.self, params: params, requestId: UUID().uuidString.lowercased())
     }
 
+    private func delete<T: WObject>(_ wobject: T) -> Promise<Void> {
+        return WAPI.delete(T.self, id: wobject.id, revision: wobject.revision)
+    }
+
     private func dumpWObject<T: WObject>(dumpType: DumpType, object: T) {
         let fileName: String
         switch dumpType {
@@ -48,9 +52,10 @@ class RevisionTest {
         }
     }
 
-    func runCreateTest() -> Promise<Void> {
+    func runCreateTest() -> Promise<(folderId: Int, listId: Int, taskId: Int)> {
         var listId = -1
         var taskId = -1
+        var folderId = -1
 
             return self.dumpAll(comment: "Before create test")
             .then { _ -> Promise<WList> in
@@ -85,26 +90,29 @@ class RevisionTest {
             }.then { note -> Promise<Void> in
                 self.dumpWObject(dumpType: .create, object: note)
                 return self.dumpAll(comment: "note created")
+            }.then { _ -> Promise<WFile> in
+                let uploadService = WUploadService()
+                let data = "Test content".data(using: .utf8)!
+                return uploadService.upload(data, filename: "TestFile.txt", for: taskId)
+            }.then { file -> Promise<Void> in
+                self.dumpWObject(dumpType: .create, object: file)
+                return self.dumpAll(comment: "file created")
             }.then {
                 self.create(from: WFolder(id: -1, title: "Test create folder", listIds: [listId]))
             }.then { folder -> Promise<Void> in
+                folderId = folder.id
                 self.dumpWObject(dumpType: .create, object: folder)
                 return self.dumpAll(comment: "folder created")
+            }.then {
+                return Promise.value((folderId, listId, taskId))
         }
     }
 
-    func runUpdateTest() -> Promise<Void>  {
-        var listId = -1
-        var taskId = -1
+    func runUpdateTest(folderId: Int, listId: Int, taskId: Int) -> Promise<Void> {
 
         return self.dumpAll(comment: "Before update test")
-            .done {
-                let list = self.dump.lists.first(where: { $0.title == "Create test list"} )!
-                listId = list.id
-                let task = self.dump.tasks.first(where: { $0.listId == listId })!
-                taskId = task.id
-            }.then { _ -> Promise<WFolder> in
-                let folder = self.dump.folders.first(where: { $0.title == "Test create folder"})!
+            .then { _ -> Promise<WFolder> in
+                let folder = self.dump.folders[folderId]!
                 var newFolder = folder
                 newFolder.title = "Test create folder updated"
                 return self.update(from: folder, to: newFolder)
@@ -188,24 +196,70 @@ class RevisionTest {
             }.then { object -> Promise<Void> in
                 self.dumpWObject(dumpType: .update, object: object)
                 return self.dumpAll(comment: "setting updated")
-            }.then { _ -> Promise<Void> in
-                let list = self.dump.lists[listId]!
-                return WAPI.delete(WList.self, id: list.id, revision: list.revision)
-            }.then {
-                self.dumpAll(comment: "list deleted")
+//            }.then { _ -> Promise<Void> in
+//                let list = self.dump.lists[listId]!
+//                return WAPI.delete(WList.self, id: list.id, revision: list.revision)
+//            }.then {
+//                self.dumpAll(comment: "list deleted")
         }
     }
 
+    func runDeleteTest(folderId: Int, listId: Int, taskId: Int) -> Promise<Void>  {
+
+        return self.dumpAll(comment: "Before delete test")
+            .then {
+                self.delete(self.dump.taskComments.first(where: { $0.taskId == taskId })!)
+            }.then {
+                return self.dumpAll(comment: "Task comment deleted")
+            }.then {
+                self.delete(self.dump.reminders.first(where: { $0.taskId == taskId })!)
+            }.then {
+                return self.dumpAll(comment: "Reminder deleted")
+            }.then {
+                self.delete(self.dump.notes.first(where: { $0.taskId == taskId })!)
+            }.then {
+                return self.dumpAll(comment: "Note deleted")
+            }.then {
+                self.delete(self.dump.files.first(where: { $0.taskId == taskId })!)
+            }.then {
+                return self.dumpAll(comment: "File deleted")
+            }.then {
+                self.delete(self.dump.subtasks.first(where: { $0.taskId == taskId })!)
+            }.then {
+                return self.dumpAll(comment: "Subtask deleted")
+            }.then {
+                self.delete(self.dump.tasks[taskId]!)
+            }.then {
+                return self.dumpAll(comment: "Task deleted")
+            }.then {
+                self.delete(self.dump.folders[folderId]!)
+            }.then {
+                return self.dumpAll(comment: "Folder deleted")
+            }.then {
+                self.delete(self.dump.lists[listId]!)
+            }.then {
+                return self.dumpAll(comment: "List deleted")
+        }
+    }
+
+
     func runTests(directory: String) {
+        var ids: (folderId: Int, listId: Int, taskId: Int) = (-1, -1, -1)
         self.directory = directory + "create/"
         log("\nCreate test\n")
         dumpContent = DumpContentComapact(directory: self.directory)
         runCreateTest()
-            .then { _ -> Promise<Void> in
+            .then { (folderId, listId, taskId) -> Promise<Void> in
+                ids = (folderId, listId, taskId)
                 log("\nUpdate test\n")
                 self.directory = directory + "update/"
                 self.dumpContent = DumpContentComapact(directory: self.directory)
-                return self.runUpdateTest()
+                return self.runUpdateTest(folderId: folderId, listId: listId, taskId: taskId)
+            }.then { _ -> Promise<Void> in
+                log("\nDelete test\n")
+                self.directory = directory + "delete/"
+                self.dumpContent = DumpContentComapact(directory: self.directory)
+                return self.runDeleteTest(folderId: ids.folderId, listId: ids.listId, taskId: ids.taskId)
             }.catch { error in
                 log(error: error)
         }
